7362bc9fb7ed668aa094bf3b895cd0da36d26873
Shivangi Sehgal <83656526+shivangisehgal@users.noreply.github.com>
2022-01-08 21:02:52 +0530

    Update 02 rotated_search.cpp

--- 10 Divide and Conquer/02 rotated_search.cpp
+++ 10 Divide and Conquer/02 rotated_search.cpp
@@ -1,57 +1,36 @@
-#include<iostream>
-#include<vector>
-using namespace std;
-
-int rotated_search(vector<int> a,int key) {
-	int n = a.size();
-
-	//Logic
-	int s = 0;
-	int e = n - 1;
-
-	while(s<=e){
-		int mid = (s+e)/2;
-
-		if(a[mid]==key){
-			return mid;
-		}
-
-		//2 cases
-		if(a[s]<=a[mid]){
-			//left
-			if(key>=a[s] and key<=a[mid]){
-				e = mid - 1;
-			}
-			else{
-				s = mid + 1;
-			}
-
-		}
-		else{
-			//right
-
-			if(key>=a[mid] and key<=a[e]){
-				s = mid + 1;
-			}
-			else{
-				e = mid - 1;
-			}
-
-		}
-
-	}
-	return -1;
-
-}
+M1 : Linear Search - TC : O(N), SC: O(1)
+M2 : Modified Binary Search - TC : O(logN), SC : O(1)
 
 
-
-int main() {
-
-	
-	vector<int> a{4,5,6,7,0,1,2,3};
-	int key;
-	cin>>key;
-	cout<< rotated_search(a,key) <<endl;
-	return 0;
-}
+class Solution {
+public:
+    int search(vector<int>& a, int target) {
+        int low = 0, high = a.size() - 1; 
+        while(low <= high) {
+            int mid = (low + high) >> 1; 
+            if(a[mid] == target) return mid; 
+            
+            // the left side is sorted
+            if(a[low] <= a[mid]) {
+                //figure out if element lies in this half, ie, a[low] <= target <= a[mid]
+                if(target >= a[low] && target <= a[mid]) {
+                    high = mid - 1; 
+                }
+                else {
+                    low = mid + 1; 
+                }
+            }
+            //right half is sorted
+            else {
+                 //figure out if element lies this half half, ie, a[mid] <= target <= a[high]
+                if(target >= a[mid] && target <= a[high]) {
+                    low = mid + 1; 
+                }
+                else {
+                    high = mid - 1; 
+                }
+            }
+        } 
+        return -1; 
+    }
+};
