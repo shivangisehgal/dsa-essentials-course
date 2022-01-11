c94cb7c65fb283e81a99226a9d3a6ee17cf7d009
Shivangi Sehgal <83656526+shivangisehgal@users.noreply.github.com>
2022-01-11 15:20:45 +0530

    added definition

--- 03 Basic Sorting /01_bubble_sort.cpp
+++ 03 Basic Sorting /01_bubble_sort.cpp
@@ -1,3 +1,9 @@
+// When you think of bubble sort, you can imagine the inputs being bubbles and rising up/going down depending on how dense they are.
+//It goes through an entire array and compares each neighboring number. 
+//It then swaps the numbers if needed and keeps doing this until the array is in ascending order.
+//The algorithm gets its name as the bigger elements will “bubble” towards the rightmost position. 
+//After each cycle, the largest element will be the last element in the unsorted part of the array. And our array size for comparison decreases by 1.
+
 //TC: O(N^2)
 //SC: O(1)
 
