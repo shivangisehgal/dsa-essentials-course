d0539fee9e6d1d4d655372a3635cf50f068e6576
Shivangi Sehgal <83656526+shivangisehgal@users.noreply.github.com>
2022-01-11 16:11:16 +0530

    added definition

--- 03 Basic Sorting /04_counting_sort.cpp
+++ 03 Basic Sorting /04_counting_sort.cpp
@@ -1,3 +1,6 @@
+// Counting sort is a sorting algorithm that sorts the elements of an array by counting the number of occurrences of each unique element in the array. 
+// The count is stored in an auxiliary array and the sorting is done by mapping the count as an index of the auxiliary array.
+
 // TC: O(N + K)
 // SC: O(N + K)
 // Where K is the range of input
