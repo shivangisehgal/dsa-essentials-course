96d78d08780b7b1830ffc4dd13b4d728da92022c
Shivangi Sehgal <83656526+shivangisehgal@users.noreply.github.com>
2022-01-11 16:04:21 +0530

    Update 02_insertion_sort.cpp

--- 03 Basic Sorting /02_insertion_sort.cpp
+++ 03 Basic Sorting /02_insertion_sort.cpp
@@ -1,3 +1,13 @@
+// Insertion sort is a sorting algorithm, which sorts the array by shifting the elements one at at time.
+// We have a sorted sublist and an unsorted sublist. It iterates the input elements by growing the sorted list at each iteration. 
+// If it is the first element, it is already sorted.
+// Pick the next element.
+// Compare with all the elements in sorted sub-list.
+// Shift all the the elements in sorted sub-list that is greater than the value to be sorted.
+// Insert the value.
+// Repeat until all the elements are in the sorted sublist.
+
+
 //TC: O(N^2)
 //SC: O(1)
 
